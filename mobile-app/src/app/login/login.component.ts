4fa9986cf18eaa4983852490d51852f646e07698
Fernando Tello <ferjo.tello@gmail.com>
2021-06-20 20:14:36 -0600

    [UPDATE] Form Group Login

--- mobile-app/src/app/login/login.component.ts
+++ mobile-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    user: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log("Todo bien")
+    }
+    else {
+      alert('Hay campos vacíos')
+    }
+
   }
 
 }
